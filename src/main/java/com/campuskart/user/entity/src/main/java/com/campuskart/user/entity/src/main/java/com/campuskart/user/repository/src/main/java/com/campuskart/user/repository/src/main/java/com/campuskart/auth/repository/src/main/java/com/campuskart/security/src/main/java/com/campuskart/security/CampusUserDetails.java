5d74692e38044fbbb1e9604f55e6d1d0ee494c50
YASH VASHISTH <vashisthyash75@gmail.com>
2026-09-15 16:12:47 +0530

    Add CampusUserDetails class implementing UserDetails

---
+++ src/main/java/com/campuskart/user/entity/src/main/java/com/campuskart/user/entity/src/main/java/com/campuskart/user/repository/src/main/java/com/campuskart/user/repository/src/main/java/com/campuskart/auth/repository/src/main/java/com/campuskart/security/src/main/java/com/campuskart/security/CampusUserDetails.java
@@ -0,0 +1,54 @@
+package com.campuskart.security;
+
+import com.campuskart.user.entity.User;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@AllArgsConstructor
+@Getter
+public class CampusUserDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName().name()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPasswordHash();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return Boolean.TRUE.equals(user.getActive());
+    }
+}
